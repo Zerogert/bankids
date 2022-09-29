55b0a646413ffdca46a64d14596a90f11bd5e73d
Zerogert <zlodey.deadalive@gmail.com>
2022-09-29 12:03:33 +0700

    add pages

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <nav class="p-1 navbar navbar-expand-lg navbar-dark bg-primary">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="navbar-nav">
+            <div class="nav-item active">
+              <button
+                class="nav-link btn btn-link"
+                @click="$router.push('AddWord')"
+              >
+                Главная
+              </button>
+            </div>
+            <div class="nav-item">
+              <button
+                class="nav-link btn btn-link"
+                @click="$router.push('ListWords')"
+              >
+                Курсы
+              </button>
+            </div>
+
+
+            <div class="nav-item">
+              <button
+                class="nav-link btn btn-link"
+                @click="$router.push('ListWords')"
+              >
+                Игра
+              </button>
+            </div>
+
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <router-view />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/sign-in',
+        name: 'sign-in',
+        component: () => import('./components/SignIn.vue')
+      },
+      {
+        path: '/game',
+        name: 'game',
+        component: () => import('./components/GamePage.vue')
+      }
+  ]
+
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
+  
+
+createApp(App).use(router).mount('#app')
